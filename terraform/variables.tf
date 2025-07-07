3909789d1b5b0ff0e1b147cb7aff5f65dfaacb40
suresh <suresh@w3logics.com>
2025-07-07 01:51:07 +0000

    Updated variables.tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,19 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "ap-south-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-0f918f7e67a3323f0"
+}
+
+variable "aws_profile" {
+  description = "AWS profile to use for authentication"
+  default     = "terra-admin"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = "t2.micro"
 }
